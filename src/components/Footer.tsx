c18d46f939f70ae6199f43813f9353e835ee2c64
Jefson Jacob <jfsnjacob@gmail.com>
2026-01-30 00:18:17 +0530

    refactor: update project references and improve news fetching logic

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="py-4 mt-auto text-sm text-center border-t text-slate-400 border-slate-200 dark:text-slate-400 dark:border-slate-700">
       <p className="mx-2">
-        CryptoTracker © {new Date().getFullYear()}{" "}
+        Trackibit © {new Date().getFullYear()}{" "}
         <a href="https://github.com/JJTK780" target="_blank" rel="author">
           Jefson
         </a>{" "}
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,7 +5,7 @@ export default function Login() {
     <div className="flex items-center justify-center min-h-screen bg-slate-100">
       <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-md">
         <h1 className="mb-6 text-2xl font-bold text-center text-slate-700">
-          Sign in to CryptoTracker
+          Sign in to Trackibit
         </h1>
         <SignIn />
       </div>
